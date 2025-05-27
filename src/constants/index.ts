4b6d8d2ab903177c3b35b356747a131304856824
Illia Bilous <bilous.job@yahoo.com>
2025-05-27 13:42:35 +0300

    feat: constants

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const API_URL = '/mock';
+
+export const mockMetricks: Metric[] = [
+  { id: 1, label: 'Users', value: 120 },
+  { id: 2, label: 'Sessions', value: 340 },
+  { id: 3, label: 'Revenue', value: '$2,000' }
+];
