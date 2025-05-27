1b17ac66926d9374daea646703dc4e59282d50ea
Illia Bilous <bilous.job@yahoo.com>
2025-05-27 13:43:18 +0300

    feat: app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router';
+
+import Sidebar from '@/components/Sidebar';
+import Overview from '@/pages/Overview';
+import Users from '@/pages/Users';
+import Settings from '@/pages/Settings';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="bg-amber-50">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 p-4">
+        <Routes>
+          <Route path="/" element={<Navigate to="/overview" />} />
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
